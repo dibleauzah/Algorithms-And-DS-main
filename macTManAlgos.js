9926afa9b9305a96ec91a2aba767582fce323800
Seif Sekalala <dibleauzah@gmail.com>
2022-06-22 02:08:50 -0400

    creation and 1st update

--- macTManAlgos.js
+++ macTManAlgos.js
@@ -1,2 +1,5 @@
 //!_Testing--JS version...(GitHub Push 1)
 
+
+
+//!_Test-2--i.e., GitHub
--- mcaTManAlgos.py
+++ mcaTManAlgos.py
@@ -1,1 +1,5 @@
-#_Testing--Py version...(GitHub Push 1)
+#//*_Testing--Py version...(GitHub Push 1)
+
+
+
+#//!_Test-2--i.e., GitHub--Py version
