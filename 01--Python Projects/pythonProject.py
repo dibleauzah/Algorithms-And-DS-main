49b9c3822d06255320d8ba13bf34f81c9c316f26
Seif Sekalala <dibleauzah@gmail.com>
2022-08-11 18:02:13 -0400

    Day 55--Aug 11

---
+++ 01--Python Projects/pythonProject.py
@@ -0,0 +1,46 @@
+
+#//?_Template 1 Starts Below:
+
+#//?--[Add Details Here]
+
+#//?_End of Template 1
+
+#//!_Template 2 Starts Below:
+
+#//*----------------------Divider------------------------//
+
+#//*_Day X: [Month] DD, 2022
+#//*_Algos & Jrl Self-Challenge 2022; Python Projects--i.e., In Lieu of Algos:
+#//*(Important Note[!!!]: Pay Attn to the Solution-Options' Complexity Analyses!)
+#//!_Template: [Name of Challenge Here]
+
+#//?--[Add Code Here]
+
+#//*----------------------Divider------------------------//
+#//* Template Ends Above (You May Delete This Line):
+
+
+#//*---------------Launch Date: Aug 11, 2022-------------//
+
+#//*_Aug 11, 2022
+#//*_Algos & Jrl Self-Challenge 2022; Python Projects--i.e., In Lieu of Algos:
+#//*(Important Note[!!!]: Pay Attn to the Solution-Options' Complexity Analyses!)
+#//!_Template: Number Guessing Game using Python
+#//!_Note: Testing suspended.
+
+# import random
+# n = random.randrange(1, 10)
+# guess = int(input("Please enter any number([!]:-)]: "))
+# while n != guess:
+#     if guess < n:
+#         print("Too low! Sorry; try again!")
+#         guess = int(input("Go on, please enter any number([!]:-)]: "))
+#     elif guess > n:
+#         print("Too high! Sorry; try again!")
+#         guess = int(input("Go on, please enter any number([!]:-)]: "))
+#     else:
+#         break
+# print("Alright! Good job! :-) ")
+
+#//*----------------------Divider------------------------//
+#//* Template Ends Above (You May Delete This Line):
