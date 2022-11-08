06be20d0e11e1556c68d206b9a36288176489b72
Seif Sekalala <dibleauzah@gmail.com>
2022-11-08 01:12:59 -0500

    Nov-8_Intvw-Practice_&_Other

---
+++ Interview-Prep/Practice/Java/practice.java
@@ -0,0 +1,7 @@
+public class Hello
+{
+    public static void main(string[] args)
+    {
+    System.out.printIn("Hello World!");
+    }
+}
---
+++ Interview-Prep/Practice/practice.js
@@ -0,0 +1,53 @@
+//*_________________Nov 8, 2022________________*//
+
+//!_#-1: Print From 1 to 255:
+// function justPrint(){
+//     for (var i = 0; i <= 255; i++){
+//         console.log(i);
+//     }
+// }
+// justPrint();
+
+//*-------------------------------------------//
+//!_#-2: Print Odds 1 to 255:
+// function printOdds(){
+//     for (var i = 1; i <= 255; i += 2){
+//         console.log(i);
+//     }
+// }
+// printOdds();
+
+//*-------------------------------------------//
+//!_#-3: Print 0 to 255 With Sums.
+// function printAndSum(){
+//     var sum = 0;
+//     for (var i = 0; i <= 255; i++){
+//         sum = sum + i;
+//         console.log (i + ' | Current Sum: ' + sum);
+//     }
+// }
+// printAndSum();
+
+//*-------------------------------------------//
+//!_#-4: Iterate Through Array, Printing Each Value.
+// function iterateThroughArray(arr){
+//     for (var i = 0; i < arr.length; i++){
+//         console.log(arr[i]);
+//     }
+// }
+
+// iterateThroughArray([1, 2, 3]);
+
+//*-------------------------------------------//
+//!_#-5: Given an array, find and print its max.
+
+// function printMax(arr){
+//     var max = 0;
+//     for (var i = 0; i < arr.length; i++){
+//         if (arr[i] > max){
+//             max = arr[i];
+//         }
+//     }
+//     console.log(max);
+// }
+// printMax([1, 2, 3, 34, 3, 5, 5, 77]);
