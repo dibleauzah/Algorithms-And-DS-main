531837b15f16b09ffb0b71dd4fcf277608a58e45
Seif Sekalala <dibleauzah@gmail.com>
2022-12-11 06:33:24 -0500

    Gen-Update_12-22

---
+++ Interview-Prep/Practice/calculator-react/src/components/KeypadComponent.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class KeyPadComponent extends Component {
+    render(){
+        return (
+            <div className="button">
+                <button name="(" onClick={e => this.props.onClick(e.target.name)}>(</button>
+                <button name="CE" onClick={e => this.props.onClick(e.target.name)}>CE</button>
+                <button name=")" onClick={e => this.props.onClick(e.target.name)}>)</button>
+                <button name="C" onClick={e => this.props.onClick(e.target.name)}
+            </div>
+        )
+    }
+}
